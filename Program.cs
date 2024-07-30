bf946a7b04279e4ab615ea5a905f3f44eca8c302
McDougall, Katie <KatieMcDougall@rocketmortgage.com>
2024-07-30 09:06:46 -0400

    Updated with more efficient code
    After some analysis of the data, I saw a few opportunities to shorten the code significantly.

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace MusicTrax
         string tag2 = "The City of Sunshine's official radio station: Sunny 107; ";
         string tag1 = "Today's hits. Yesterday's favorites. Sunny 107; ";
         string tag3 = "Sunny 107: the Basin's soft rock station; ";
+        string tag4 = "Today's hits; Timeless favorites. Sunny 107;";
 
         [STAThread]
         private static void Main(string[] args)
@@ -69,143 +70,107 @@ namespace MusicTrax
             StreamWriter trackWriter = new StreamWriter(script, false);
             trackWriter.WriteLine($"KKRB Script for {logDate.ToString("ddd")}, {logDate.ToString("M-d-yy")}");
             string whichTag = "";
+            int iHour;
 
             // To read a text file line by line 
             if (File.Exists(inFile))
             {
                 // Store each line in array of strings 
                 string[] lines = File.ReadAllLines(inFile);
-                foreach (string line in lines)
+                for (int i = 0; i < lines.Length; i++)
                 {
-                    string[] fields = line.Split(',');
-                    for (int i = 0; i < fields.Count(); i++)
+                    string[] fields = lines[i].Split(',');
+                    iHour = GetHour(lines[i]);
+                    for (int j = 0; j < fields.Count(); j++)
                     {
-                        fields[i] = fields[i].Replace('"', ' ');
+                        fields[j] = fields[j].Replace('"', ' ');
                     }
-                    if (!line.Contains("SWEEPER") &&
-                       (line.StartsWith("06:") ||
-                        line.StartsWith("07:") ||
-                        line.StartsWith("08:") ||
-                        line.StartsWith("09:") ||
-                        line.StartsWith("10:") ||
-                        line.StartsWith("11:") ||
-                        line.StartsWith("12:") ||
-                        line.StartsWith("13:") ||
-                        line.StartsWith("14:") ||
-                        line.StartsWith("15:") ||
-                        line.StartsWith("16:") ||
-                        line.StartsWith("17:")))
+                    if (!lines[i].Contains("SWEEPER") &&
+                       (iHour >= 6 && iHour < 18))
                     {
                         if (fields[1] == "*")
                         {
                             trackWriter.WriteLine($"");
                             trackWriter.WriteLine($"***************** {fields[0]} HOUR *****************");
                         }
-                        else if (line.Contains("JV06") ||
-                                 line.Contains("JV07") ||
-                                 line.Contains("JV08") ||
-                                 line.Contains("JV09") ||
-                                 line.Contains("JV10") ||
-                                 line.Contains("JV11") ||
-                                 line.Contains("JV12") ||
-                                 line.Contains("JV13") ||
-                                 line.Contains("JV14") ||
-                                 line.Contains("JV15") ||
-                                 line.Contains("JV16") ||
-                                 line.Contains("JV17") ||
-                                 line.Contains("JN06") ||
-                                 line.Contains("JN07") ||
-                                 line.Contains("JN08") ||
-                                 line.Contains("JN09") ||
-                                 line.Contains("JN10") ||
-                                 line.Contains("JN11") ||
-                                 line.Contains("JN12") ||
-                                 line.Contains("JN13") ||
-                                 line.Contains("JN14") ||
-                                 line.Contains("JN15") ||
-                                 line.Contains("JN16") ||
-                                 line.Contains("JN17") ||
-                                 line.Contains("JNTIME"))
+                        if (lines[i].Contains("VOICETRACK") ||
+                             lines[i].Contains("Voice Track"))
                         {
+                            whichTag = SelectTag(rando.Next(7, 2501));
                             trackWriter.WriteLine($"");
                             trackWriter.WriteLine($"{fields[3]}");
-                            if (!line.Contains("JV0620") &&
-                                !line.Contains("JV0720") &&
-                                !line.Contains("JV0820") &&
-                                !line.Contains("JV0920") &&
-                                !line.Contains("JV1020") &&
-                                !line.Contains("JV1120") &&
-                                !line.Contains("JV1220") &&
-                                !line.Contains("JV1320") &&
-                                !line.Contains("JV1420") &&
-                                !line.Contains("JV1520") &&
-                                !line.Contains("JV1620") &&
-                                !line.Contains("JV1720"))
-                            {
-                                int randoNum = rando.Next();
-                                if (randoNum % 3 == 0)
-                                {
-                                    // "Today's hits. Yesterday's favorites. Sunny 107; "
-                                    whichTag = tag1;
-                                }
-                                else if (randoNum % 2 == 0)
-                                {
-                                    // "The City of Sunshine's official radio station: Sunny 107; "
-                                    whichTag = tag2;
-                                }
-                                else
-                                {
-                                    // "Sunny 107: the Basin's soft rock station; "
-                                    whichTag = tag3;
-                                }
-                                trackWriter.WriteLine($"----------------");
-                                trackWriter.WriteLine(whichTag);
-                                trackWriter.WriteLine();
-                            }
-                            else if (line.Contains("JV0620") ||
-                                     line.Contains("JV0720") ||
-                                     line.Contains("JV0820") ||
-                                     line.Contains("JV0920") ||
-                                     line.Contains("JV1020") ||
-                                     line.Contains("JV1120") ||
-                                     line.Contains("JV1220") ||
-                                     line.Contains("JV1320") ||
-                                     line.Contains("JV1420") ||
-                                     line.Contains("JV1520") ||
-                                     line.Contains("JV1620") ||
-                                     line.Contains("JV1720"))
-                            {
-                                trackWriter.WriteLine("------------");
-                                trackWriter.WriteLine(":20 BREAK TAGLINE HERE ");
-                                trackWriter.WriteLine();
-                            }
+                            trackWriter.WriteLine($"----------------");
+                            trackWriter.WriteLine(whichTag);
+                            trackWriter.WriteLine();
+
                         }
-                        else if (line.Contains("STOPSET"))
+                        else if (lines[i].Contains("STOPSET"))
                         {
                             trackWriter.WriteLine($"STOPSET");
                         }
                         else
                         {
-                            trackWriter.WriteLine($"{fields[0]} {fields[3]} {fields[4]} {fields[5]} {fields[6]} {fields[7]}");
+                            if (!lines[i].Contains("Legal ID"))
+                            {
+                                trackWriter.WriteLine($"{fields[0]} {fields[3]} {fields[4]} {fields[5]} {fields[6]} {fields[7]}");
+                            }
                         }
                     }
 
                 }
             }
-            trackWriter.WriteLine();
-            trackWriter.WriteLine("*******************************************************");
-            trackWriter.WriteLine("**************KATIE: TURN ON YOUR FRIDGE!**************");
             trackWriter.Flush();
             trackWriter.Close();
             trackWriter.Dispose();
             // Console.ReadLine();
-
 /*
             if (File.Exists(inFile))
             {
                 File.Delete(inFile);
             }
 */
-        }
+        } // Process Text
+        private string SelectTag(int randomTag)
+        {
+
+            bool lIsPrime = IsPrime(randomTag);
+            if (lIsPrime)
+            {
+                return tag4;
+            }
+            else if (randomTag % 2 == 0)
+            {
+                return tag2;
+            }
+            else if (randomTag % 3 == 0)
+            {
+                return tag3;
+            }
+            return tag1;
+        } // Select Tag
+
+        private bool IsPrime(int whatNumber)
+        {
+            for (int i = 8; i < whatNumber; i++)
+                if (whatNumber % i == 0) return false;
+            return true;
+        } // IsPrime
+
+        private int GetHour(string line)
+        {
+            int hour = 0;
+            string subst = "";
+            if (line != null &&
+                line.Length > 0)
+            {
+                subst = line.Substring(0, 2);
+            }
+
+            if (int.TryParse(subst, out hour))
+            {
+                return hour;
+            }
+            return 0;
+        } // GetHour
     } // class
 } // namespace
